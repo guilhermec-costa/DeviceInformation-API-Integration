fa926b527d09c1c3a8a0e59d21d66296e8ab5a05
Guilherme China <echina725@gmail.com>
2023-06-01 21:45:56 -0300

    gitignore atualizado e sinal de diferença

--- request_options/by_deveui_text.py
+++ request_options/by_deveui_text.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def deveui_forms(header, fields_to_search):
     dev_digitados = st.text_area('Digite deveuis nesse campo separados por vírgula:').split(',')
-    if dev_digitados is not "":
+    if dev_digitados != "":
         with st.form(key='get_by_deveui'):
             if st.form_submit_button(label='Iniciar requisição'):
                 data = get_by_deveui_remake(dev_digitados, header=header, fields=fields_to_search)
